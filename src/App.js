b842d7aab85d1306afa0f39b04bf2224dbc75a22
Heather <h.vanderberg92@gmail.com>
2021-08-12 20:32:47 -0500

    Create React Weather App using REACT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./weather.css";
+import Temperature from "./Temperature";
+import Humidity from "./Humidity";
+import Wind from "./Wind";
+import bootstrap from `bootstrap`;
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body class="weatherApp">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <input
+                type="City/State"
+                id="inputLocation"
+                class="form-control"
+                aria-describedby="locationHelpBlock"
+                placeholder="Enter City/State then select Search"
+              />
+              <br />
+              <span class="btn" id="searchBtn">
+                <input type="Submit" value="Search" />
+              </span>
+              <span class="btn" id="currentBtn">
+                <input type="Submit" value="Current" />
+              </span>
+            </div>
+            <div class="row currentDay">
+              <div class="col currentIcon">
+                <div>
+                  <img
+                    src="https://openweathermap.org/img/wn/10d@2x.png"
+                    alt="Weather"
+                    id="icon"
+                  />
+                  <span class="temperature" id="temperature">
+                    <Temperature />
+                  </span>
+                  <span>℉</span>
+                </div>
+              </div>
+              <div class="col currentInfo">
+                <div id="currentLocation">New York</div>
+                Last Updated:
+                <div id="currentTime">Sunday, 12:00pm</div>
+              </div>
+              <div class="col addtInfo">
+                <div id="currentCondition">Clear, Sunny</div>
+                <span id="wind">
+                  <Wind />
+                </span>
+                <span id="humidity">
+                  <Humidity />
+                </span>
+              </div>
+              <div id="fiveDays"></div>
+            </div>
+          </div>
+          <br />
+          <a href="https://github.com/hvander-capm/my-weather-project">
+            Open Source Code by Heather Vanderberg
+          </a>
+        </div>
+      </body>
     </div>
   );
 }
-
-export default App;
---
+++ src/Wind.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Wind(props) {
+  return <div>Wind: {props.value}Mph</div>;
+}
